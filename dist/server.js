421209e977ac1cb6aa7cc2a0a44a6e170c8c4a77
saurabhraut1212 <saurabhrmahadev1212@gmail.com>
2025-05-17 12:06:35 +0530

    feat: initial setup for blog post

--- dist/server.js
+++ dist/server.js
@@ -6,5 +6,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = __importDefault(require("./index"));
 const PORT = process.env.PORT || 5000;
 index_1.default.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running ${PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import app from "./index";
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running ${PORT}`);
 });
